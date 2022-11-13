9f3447b14ee0cf5e5f004713817680686cc33ef8
xXDjMikeXx <versace.2006@gmx.at>
2022-11-13 10:20:49 +0100

    Update mqtt-install.sh

--- setup/mqtt-install.sh
+++ setup/mqtt-install.sh
@@ -62,7 +62,7 @@ set +e
 alias die=''
 if nc -zw1 8.8.8.8 443; then msg_ok "Internet Connected"; else
   msg_error "Internet NOT Connected"
-  exit 1
+#  exit 1
 fi
 RESOLVEDIP=$(nslookup "github.com" | awk -F':' '/^Address: / { matched = 1 } matched { print $2}' | xargs)
 if [[ -z "$RESOLVEDIP" ]]; then msg_error "DNS Lookup Failure"; else msg_ok "DNS Resolved github.com to $RESOLVEDIP"; fi
